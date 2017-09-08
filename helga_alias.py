d9dad4ae6c36c1117e73768a3d48a9137dc19d1f
Justin Caratzas <bigjust@lambdaphil.es>
2017-09-08 12:25:47 -0400

    add function to determine if a token is an alias

--- helga_alias.py
+++ helga_alias.py
@@ -20,6 +20,15 @@ logger = log.getLogger(__name__)
 # }
 
 
+def is_alias(potential_nick):
+
+    for result in db.alias.find():
+        if potential_nick in result['aliases']:
+            return True
+
+    return False
+
+
 def find_alias(nick, create_new=True):
     """
     Returns a tuple containing the key nick for an alias given, and
